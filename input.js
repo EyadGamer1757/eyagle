071a89fa6cf6ff64c7cb253d6ca4e4b21dcdf4c3
EyadGamer1757 <166323143+EyadGamer1757@users.noreply.github.com>
2024-06-05 20:05:45 +0300

    Update input.js

--- input.js
+++ input.js
@@ -3,3 +3,8 @@ function search() {
     const searchUrl = `https://www.google.com/search?q=${encodeURIComponent(query)}`;
     window.open(searchUrl, '_blank');
 }
+
+function openLink(){
+    var input = document.getElementById('userInput').value;
+    window.location.href = 'https://' + input;
+}
